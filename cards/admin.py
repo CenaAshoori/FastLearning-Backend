e8dcfcb305ceb5f88292638e094fd68194425ed9
Cena Ashoori <52758031+CenaAshoori@users.noreply.github.com>
2023-06-04 18:15:24 +0330

    add update field and news and category

--- cards/admin.py
+++ cards/admin.py
@@ -19,8 +19,13 @@ class CardImageInline(admin.TabularInline):
     model = CardModel.images.through  # specifies the through model for the many-to-many relationship
     extra = 0  # sets the minimum number of extra forms to display
 
+from django.db import models
+
 @admin.register(CardModel)
 class CardModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': admin.widgets.AdminRadioSelect},
+    }
     inlines = [CardImageInline]
     form = MyModelForm
 
--- cards/models.py
+++ cards/models.py
@@ -1,6 +1,8 @@
 
 import uuid
 from django.db import models
+from django.utils import timezone
+
 # from accounts.models import User
 def get_file_path(instance, filename):
     extension = filename.split('.')[-1]
@@ -35,6 +37,29 @@ class CardModel(models.Model):
     images = models.ManyToManyField(CardImage)
     file = models.FileField(upload_to=get_file_path)
     video_link = models.URLField(blank=True, null=True)
+
+    NEWS = 'NW'
+    FOLDER = 'FL'
+    BOOK = 'BK'
+    LESSON = 'LE'
+    CATEGORY_CHOICES = [
+        (NEWS, 'News'),
+        (FOLDER, 'Folders'),
+        (BOOK, 'Books'),
+        (LESSON, 'Lessons'),
+    ]
+    category_type = models.CharField(
+        max_length=2,
+        choices=CATEGORY_CHOICES,
+        default=BOOK,
+    )
+    isNews = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        # update the timestamp whenever the object is saved
+        self.updated_at = timezone.now()
+        return super(CardModel, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
